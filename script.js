65ca142980b27172d5bd94d57f8aab803c283d26
Kirito72-hub <ahmed.saudi1996@gmail.com>
2024-08-13 09:55:26 +0300

    wrote js code that print 16x16 box

---
+++ script.js
@@ -0,0 +1,8 @@
+// used querySelector here with .container cuz the selectClassByName get me array and that dosnt work with append appendChiled
+const container = document.querySelector(".container");
+// for loop that write 16x16 box and give them class name and add them to the main container which is in html file
+for(let i=0; i<256; i++){
+    const box = document.createElement("div");
+    box.className = "box";
+        container.appendChild(box);
+}
