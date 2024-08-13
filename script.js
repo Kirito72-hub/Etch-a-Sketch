ae5bc2180a029f536ca6674496b0a63893277f64
Kirito72-hub <ahmed.saudi1996@gmail.com>
2024-08-13 10:16:43 +0300

    styled the page using css and grid display to make the boxes columns and rows 16x16

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const container = document.querySelector(".container");
 for(let i=0; i<256; i++){
     const box = document.createElement("div");
     box.className = "box";
-        container.appendChild(box);
+    container.appendChild(box);
 }
