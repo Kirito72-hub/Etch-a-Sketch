268fde4450b8619a944792f5540423ab4eee87fe
Kirito72-hub <ahmed.saudi1996@gmail.com>
2024-08-14 10:35:14 +0300

    pressing the button now recreate grid with nummber of boxes the output is a little bit wrong

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const container = document.querySelector(".container");
 
 //animation for hovering and pressing the button
-const btn = document.querySelector(".btn");
+const btn = document.getElementById("btn");
 btn.addEventListener("mouseover", (event)=>{
     event.target.style.background = "grey";
 });
@@ -19,22 +19,31 @@ btn.addEventListener("mouseup", (event)=>{
 
 
 // for loop that write 16x16 box and give them class name and add them to the main container which is in html file
-for(let i=0; i<256; i++){
-    const box = document.createElement("div");
-    box.classList.add("box");
-      // Mouse over event to change color to red
-      box.addEventListener("mouseover", (event) => {
-        event.target.style.background = "red";
-    });
-
-    // Mouse out event to change color back to aqua
-    box.addEventListener("mouseout", (event) => {
-        event.target.style.background = "aqua"; // Reset to original color
-    });
-    container.appendChild(box);
+function makeGrid(input){
+    for(let i=0; i<input; i++){
+        const box = document.createElement("div");
+        box.classList.add("box");
+          // Mouse over event to change color to red
+          box.addEventListener("mouseover", (event) => {
+            event.target.style.background = "red";
+        });
+    
+        // Mouse out event to change color back to aqua
+        box.addEventListener("mouseout", (event) => {
+            event.target.style.background = "aqua"; // Reset to original color
+        });
+        container.appendChild(box);
+    }
 }
 //add button using DOM
 //buton on press function
 //pop up that gets integer smaller than 100
 //recreate the grid to suit the new grid inputted by the user 
 
+btn.onclick = function(){
+    const value = prompt("enter grid number less than 100 ?");
+    if(value<=100 && value>0)
+        makeGrid(value);
+    else
+        alert("Please enter a value between 1-100 !");
+};
