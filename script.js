623beec0d7395dce66a9eaf58b5dc38dcd4155e9
Kirito72-hub <ahmed.saudi1996@gmail.com>
2024-08-14 08:32:46 +0300

    added event listener with hoverover and hoverout

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ const container = document.querySelector(".container");
 // for loop that write 16x16 box and give them class name and add them to the main container which is in html file
 for(let i=0; i<256; i++){
     const box = document.createElement("div");
-    box.className = "box";
+    box.classList.add("box");
+      // Mouse over event to change color to red
+      box.addEventListener("mouseover", (event) => {
+        event.target.style.background = "red";
+    });
+
+    // Mouse out event to change color back to aqua
+    box.addEventListener("mouseout", (event) => {
+        event.target.style.background = "aqua"; // Reset to original color
+    });
     container.appendChild(box);
 }
+
+//mouse hover event listener
